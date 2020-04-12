d03bef1111d116424e6441508b368ae37f9a8e6b
Satish Sahu <satisahu@gmail.com>
2020-04-12 08:31:46 +0530

    Added datastore connectivity initial version

--- src/main/kotlin/com/abhi/datastoreconnect/Main.kt
+++ src/main/kotlin/com/abhi/datastoreconnect/Main.kt
@@ -1,25 +1,28 @@
 package com.abhi.datastoreconnect
 
+import com.abhi.datastoreconnect.datastore.checkConnectivity
 import java.lang.Exception
 
 fun main(){
-
+    checkConnectivity()
     println("Please select what you want to deal with!!")
     println("1. Namespace")
     println("2. Kind")
     println("3. Entity")
 
     val a= print("Please enter the numerical choice here: ")
-    val choice = readLine()
-    try {
-        val selectedChice = choice!!.toInt()
-        println("selected choice is $selectedChice")
+    val i:Int=0
+    while (i == 0) {
+        try {
+            val choice = readLine()
+            val selectedChice = choice!!.toInt()
+            println("selected choice is $selectedChice")
+            break
+        } catch (e: Exception) {
+            println("Please enter numeric")
+            continue
+        }
     }
-    catch (e : Exception){
-        println("Please enter numeric")
-
-    }
-
     println(a)
     val b= readLine()
     println(b)
--- src/main/kotlin/com/abhi/datastoreconnect/datastore/DatastoreConnectivity.kt
+++ src/main/kotlin/com/abhi/datastoreconnect/datastore/DatastoreConnectivity.kt
@@ -1,2 +1,26 @@
 package com.abhi.datastoreconnect.datastore
 
+import com.google.cloud.datastore.*
+
+
+fun checkConnectivity(){
+        try {
+            val dataStore = DatastoreOptions.newBuilder()
+                .build().service
+
+            val query: Query<Key> = Query.newKeyQueryBuilder()
+                .setKind("_namespace_")
+                .build()
+
+            val namespaces = ArrayList<String>();
+            val results = dataStore.run(query);
+            while (results.hasNext()) {
+                namespaces.add(results.next().getName());
+            }
+            print(namespaces)
+        } catch (e: DatastoreException) {
+            println(e)
+        }
+
+    }
+
