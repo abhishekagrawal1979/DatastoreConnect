6519e8bf0db501286761f4824784ee928965c84a
abhishek agrawal <abhishek.agrawal@johnlewis.co.uk>
2020-04-12 04:46:59 +0100

    add test file

---
+++ src/test/kotlin/com/abhi/datastoreconnect/datastore/DataStoreConnectivityTests.kt
@@ -0,0 +1,12 @@
+package com.abhi.datastoreconnect.datastore
+
+import org.junit.jupiter.api.Test
+
+class DataStoreConnectivityTests {
+
+    @Test
+    fun `this is a test test`(){
+        println("this is my first test")
+    }
+
+}
