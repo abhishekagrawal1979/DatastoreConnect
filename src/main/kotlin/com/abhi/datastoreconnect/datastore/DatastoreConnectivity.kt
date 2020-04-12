afc6985f46575c8866c75bdc199cdc2ec1aec171
abhishek agrawal <abhishek.agrawal@johnlewis.co.uk>
2020-04-12 04:08:34 +0100

    Initial commit

--- src/main/kotlin/com/abhi/datastoreconnect/datastore/DatastoreConnectivity.kt
+++ src/main/kotlin/com/abhi/datastoreconnect/datastore/DatastoreConnectivity.kt
@@ -1,16 +1,16 @@
 package com.abhi.datastoreconnect.datastore
 
 import com.google.cloud.datastore.*
+import java.util.ArrayList
 
 
 fun checkConnectivity(){
         try {
             val dataStore = DatastoreOptions.newBuilder()
-                .build().service
-
+                    .build().service
             val query: Query<Key> = Query.newKeyQueryBuilder()
-                .setKind("_namespace_")
-                .build()
+                    .setKind("__namespace__")
+                    .build()
 
             val namespaces = ArrayList<String>();
             val results = dataStore.run(query);
