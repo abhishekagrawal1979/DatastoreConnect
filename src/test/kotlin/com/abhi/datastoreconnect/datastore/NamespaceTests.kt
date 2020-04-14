fb9a9ef68dcf4ed7496773612a84770424273dab
abhishek agrawal <abhishek.agrawal@johnlewis.co.uk>
2020-04-14 13:36:16 +0100

    add test file

--- src/test/kotlin/com/abhi/datastoreconnect/datastore/NamespaceTests.kt
+++ src/test/kotlin/com/abhi/datastoreconnect/datastore/NamespaceTests.kt
@@ -5,23 +5,23 @@ import io.mockk.mockk
 import io.mockk.verify
 import org.junit.jupiter.api.Test
 
-class DatastoreRepositoryTests() {
+
+class NamespaceTests {
+
     @Test
     fun `this is a getNamespace test`() {
 
         val mockDatastoreRepository = mockk<DatastoreRepository>()
-//        val mockNamespace = mockk<Namespace>()
+
         every { mockDatastoreRepository.getMetadataList("__namespace__", "", "") } returns arrayListOf(
             "Abhinamespace",
             "Satishnamespace"
         )
         val objNamespace = Namespace()
 
-        every { objNamespace.getAll() } returns Unit
-        mockDatastoreRepository.getMetadataList("__namespace__", "", "")
         objNamespace.getAll()
-
         verify { mockDatastoreRepository.getMetadataList("__namespace__", "", "") }
 
     }
+
 }
